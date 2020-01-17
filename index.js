71e553bab23359c28920c983d250a24bd3b64aa4
KrisCastillo <criscastillo2@hotmail.com>
2020-01-17 12:11:34 -0500

    Event Listeners

---
+++ index.js
@@ -0,0 +1,29 @@
+function addFormControls() {
+    $('#js-shopping-list-form').submit(event=>{
+      event.preventDefault();
+        const item = $('#shopping-list-entry').val();
+        $('.shopping-list').append(` 
+        <li>
+        <span class="shopping-item">${item}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`)
+    })
+  
+  $('ul').on('click', '.shopping-item-delete', function (event) {
+    console.log('Here in remove event listener');
+    $(this).closest('li').remove();
+  });
+  $('ul').on('click', '.shopping-item-toggle',function(event){
+    console.log('Here in toggle event listener');
+    $(this).parent().siblings('span').toggleClass('shopping-item__checked');
+  });
+  }
+  
+  $(addFormControls);
